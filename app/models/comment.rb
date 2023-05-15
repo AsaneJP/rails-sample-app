b9f68861ef14b98655d056f9e1e59287024d069d
AsaneKento <74521188+AsaneJP@users.noreply.github.com>
2023-05-16 01:05:33 +0900

    コメントのテーブルを作成

---
+++ app/models/comment.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :bigint           not null, primary key
+#  comment    :text             not null
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  board_id   :bigint
+#
+# Foreign Keys
+#
+#  fk_rails_...  (board_id => boards.id)
+#
+class Comment < ApplicationRecord
+  belongs_to :board
+end
---
+++ test/models/comment_test.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :bigint           not null, primary key
+#  comment    :text             not null
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  board_id   :bigint
+#
+# Foreign Keys
+#
+#  fk_rails_...  (board_id => boards.id)
+#
+require 'test_helper'
+
+class CommentTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
