6a8aa7b5ba9d8cfb8d6be6052ec37e19ad8b5b2b
AsaneKento <74521188+AsaneJP@users.noreply.github.com>
2023-05-16 00:26:40 +0900

    ページネーションの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 if Rails.env == 'development'
-  (1..50).each do |i|
+  (1..100).each do |i|
     Board.create(name: "ユーザー#{i}", title: "タイトル#{i}", body: "本文#{i}")
   end
 end
