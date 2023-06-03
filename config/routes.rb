a4033566dc5e4ae9a135147c35fa75c2dfe627f0
AsaneKento <74521188+AsaneJP@users.noreply.github.com>
2023-06-03 11:02:29 +0900

    ユーザーログインの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'mypage', to: 'users#me'
 
-  get 'login', to: 'sessions#create'
+  post 'login', to: 'sessions#create'
 
-  get 'logout', to: 'sessions#destroy'
+  delete 'logout', to: 'sessions#destroy'
 
   root 'home#index'
   resources :users, only: %i[new create]
